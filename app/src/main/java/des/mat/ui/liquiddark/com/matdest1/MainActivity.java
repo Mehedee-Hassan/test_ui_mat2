64edb2087af4dc0bcfda67bea39af88553548160
Mehedee Hassan <razin.meha@gmail.com>
2017-12-14 12:11:14 +0600

    toolbar and drop down menu

--- app/src/main/java/des/mat/ui/liquiddark/com/matdest1/MainActivity.java
+++ app/src/main/java/des/mat/ui/liquiddark/com/matdest1/MainActivity.java
@@ -1,13 +1,53 @@
 package des.mat.ui.liquiddark.com.matdest1;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.item1_main_m){
+
+            Toast.makeText(MainActivity.this,"item 1 clicked" , Toast.LENGTH_SHORT).show();
+
+        }else if(itemId == R.id.item2_main_m){
+            Toast.makeText(MainActivity.this,"item 2 clicked" , Toast.LENGTH_SHORT).show();
+
+        }else if(itemId == R.id.item3_main_m){
+            Toast.makeText(MainActivity.this,"item 3 clicked" , Toast.LENGTH_SHORT).show();
+
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
